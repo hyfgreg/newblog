e503cc8ded5c124cbf57e4a98b961626143a1aab
hyfgreg <hyfgreg@163.com>
2017-11-01 18:52:44 +0800

    fisrt add with basic structure

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 465
+    # MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'hyfgreg'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Zhangxia2008111'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <hyfgreg@163.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'hyfgreg@163.com'
+    # FLASKY_POSTS_PER_PAGE = 20
+    # FLASKY_COMMENTS_PER_PAGE = 20
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql://root:admin@localhost/flask_dev'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'mysql://root:admin@localhost/flask_test'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql://root:admin@localhost/flask'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
