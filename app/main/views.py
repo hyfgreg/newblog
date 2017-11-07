9fdf820da23f247463e108208c8228b72f0fa2ea
hyfgreg <hyfgreg@163.com>
2017-11-07 19:47:22 +0800

    add main.index with post

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, session, redirect, url_for, current_app,request
 from .. import db
-from ..models import User
+from ..models import User,Post
 from ..email import send_email
 from . import main
 from .forms import NameForm
@@ -13,7 +13,9 @@ def index():
     # # print(session['name_hash'])
     # name_hash_sess = session.get('name_hash',None)
     # if name_hash and name_hash_sess and name_hash == name_hash_sess:
-    if check_login():
-        return render_template('index.html',
-                           name =session.get('username'))
-    return render_template('index.html')
+    # if check_login():
+    #     return render_template('index.html',
+    #                        name =session.get('username'))
+
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html',name = session.get('username'),posts = posts)
